b273aa19073f8523581558f9072084052a028b8d
Dipu Haider <dipuhaider@gmail.com>
2023-11-07 15:32:01 +0600

    Login & Registration with Firebase Authentication

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../layouts/Root";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Home from "../pages/home/Home";
+import Login from "../auth/Login/Login";
+import Register from "../auth/Register/Register";
 
 const router = createBrowserRouter([
     {
@@ -13,6 +15,14 @@ const router = createBrowserRouter([
                 path: '/',
                 element: <Home></Home>,
                 //loader: () => fetch('https://m10a-brand-shop-server-283gyzcf7-dipuhaiders-projects.vercel.app/brand'),
+            },
+            {
+                path: '/login',
+                element: <Login></Login>,
+            },
+            {
+                path: '/register',
+                element: <Register></Register>,
             },
         ]
     }
