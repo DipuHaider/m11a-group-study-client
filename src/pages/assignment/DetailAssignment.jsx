3eb5eea3ab6a2b0ffdab6025997241b9cfd0d7e8
Dipu Haider <dipuhaider@gmail.com>
2023-11-09 12:58:27 +0600

    Develop Taken Assignment

--- src/pages/assignment/DetailAssignment.jsx
+++ src/pages/assignment/DetailAssignment.jsx
@@ -1,35 +1,65 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
 import { useContext } from "react";
 import { AuthContext } from '../../providers/AuthProvider';
 import Swal from 'sweetalert2';
 
 const DetailAssignment = () => {
 
+    const navigate = useNavigate();
+
+    //Logged in user info
     const { user } = useContext(AuthContext);
 
+    //Get data of assignment using loader data & destructure
     const assignment = useLoaderData();
     const { _id, title, description, marks, thumbnail, level, useremail } = assignment;
 
-    const handleTaken = title  => {
-    
-        const form = title.target;
+    //get taken status
+    const [takenAssignments, setAssignments] = useState([]);
 
+    useEffect(() => {
+        
+        fetch("http://localhost:5000/taken")
+            .then((response) => response.json())
+            .then((data) => {
+                setAssignments(data);
+            })
+            .catch((error) => {
+                console.error("Error fetching taken assignment data:", error);
+            });
+    }, []);
+
+   //function to check if an assignment is taken
+    const isAssignmentTaken = (_id, userEmail, takenAssignments) => {
+        return takenAssignments.some(takenAssignment => {
+        return takenAssignment.assignmentid === _id && takenAssignment.assignmentTaker === user.email;
+        });
+    };
+    const isTaken = isAssignmentTaken(_id, user.email, takenAssignments);
+    console.log(isTaken);
+
+    const handleTaken = (pdflink, note)  => {
+    
+        const assignmentid = _id;
         const assignmentName = title;
         const assignmentOwner = useremail;
         const assignmentTaker = user.email;
         const assignmentStatus = "pending";
-        const assignmentPdfLink = form.pdflink.value;
-        const assignmentNote = useremail;
+        const assignmentPdfLink = pdflink;
+        const assignmentNote = note;
+
+        console.log(assignmentPdfLink)
 
 
-        const addedAssignmnent = { assignmentName, assignmentOwner, assignmentTaker, assignmentStatus, assignmentPdfLink, assignmentNote }
+        const addedAssignment = { assignmentid, assignmentName, assignmentOwner, assignmentTaker, assignmentStatus, assignmentPdfLink, assignmentNote }
 
         fetch('http://localhost:5000/taken', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(addedAssignmnent)
+            body: JSON.stringify(addedAssignment)
         })
             .then(res => res.json())
             .then(data => {
@@ -43,6 +73,9 @@ const DetailAssignment = () => {
                     })
                 }
             })
+        setTimeout(() => {
+                navigate('/allassignment');
+            }, 2000);
     }
 
     return (
@@ -56,30 +89,52 @@ const DetailAssignment = () => {
                         <p className="justify-start text-lg text-slate-900">{level}</p>
                         <p className="justify-start text-lg text-slate-900">${marks}</p>
                         <p className="justify-start text-base text-slate-900">{description}</p>
-                        <p><button onClick={() => handleTaken(title)} className="btn btn-md bg-theme-primary text-white hover:text-theme-primary hover:bg-blue-200 hover:border-theme-primary">Take Assignment</button></p>
+                        
 
-                        {/* You can open the modal using document.getElementById('ID').showModal() method */}
-<button className="btn" onClick={()=>document.getElementById('my_modal_3').showModal()}>open modal</button>
-<dialog id="my_modal_3" className="modal">
-  <div className="modal-box">
-    <form method="dialog">
-      {/* if there is a button in form, it will close the modal */}
-      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-    </form>
-    <form onSubmit={handleTaken}  className="space-y-2">
-        <div>
-            <label className="label">
-                <span className="text-base label-text">Assignment Title</span>
-            </label>
-            <input type="text" name="pdflink" placeholder="title" className="w-full input input-bordered input-primary border-theme-primary" />
-        </div>
-        <div>
-            <button className="btn btn-block bg-theme-primary text-white hover:text-theme-primary hover:bg-blue-200 hover:border-theme-primary">Take Assignment</button>
-        </div>
-    </form>
-  </div>
-</dialog>
+                        {
+                            isTaken ? <>
+                                <h4 className='text-red-500'>Assignment taken already</h4>
+                                <button className="btn" onClick={()=>document.getElementById('my_modal_3').showModal()} disabled>Take Assignment</button>
+                                </>
+                            : 
+                            <>
+                                <button className="btn" onClick={()=>document.getElementById('my_modal_3').showModal()}>Take Assignment</button>
+                            </>
+                        }
 
+                        <dialog id="my_modal_3" className="modal">
+                        <div className="modal-box">
+                            <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                            </form>
+                                <form
+                                    onSubmit={(e) => {
+                                        e.preventDefault(); // Prevent the default form submission
+                                        const pdflink = e.target.pdflink.value;
+                                        const note = e.target.note.value;
+                                        handleTaken(pdflink, note); // Call your handleTaken function
+                                    }}
+                                    className="space-y-2"
+                                    >
+                                <div>
+                                    <label className="label">
+                                        <span className="text-base label-text">Assignment Title</span>
+                                    </label>
+                                    <input type="text" name="pdflink" placeholder="title" className="w-full input input-bordered input-primary border-theme-primary" />
+                                </div>
+                                <div>
+                                    <label className="label">
+                                        <span className="text-base label-text">Assignment Note</span>
+                                    </label>
+                                    <input type="text" name="note" placeholder="note" className="w-full input input-bordered input-primary border-theme-primary" />
+                                </div>
+                                <div>
+                                    <button onClick={() => { const modal = document.getElementById('my_modal_3'); modal.close(); }} className="btn btn-block bg-theme-primary text-white hover:text-theme-primary hover:bg-blue-200 hover:border-theme-primary">Take Assignment</button>
+                                </div>
+                            </form>
+                        </div>
+                        </dialog>
 
                     </div>
                 </div>
